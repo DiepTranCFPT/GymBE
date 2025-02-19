a8004b338764286d35874fed497f29897742e1d2
DIEP TRAN <trancaodiepnct@gmail.com>
2025-02-19 13:34:59 +0700

    add

--- src/main/java/com/example/demo/NewExperessApplication.java
+++ src/main/java/com/example/demo/NewExperessApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 
+import com.example.demo.config.FirebaseProperties;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
@@ -8,11 +9,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
-
+@EnableConfigurationProperties(FirebaseProperties.class)
 public class NewExperessApplication {
 
 	public static void main(String[] args) {
