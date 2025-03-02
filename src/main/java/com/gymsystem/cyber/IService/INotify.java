093a0863ae868c5088536df4b4b261a5a4f8ac5e
DIEP TRAN <152488904+DiepTranCFPT@users.noreply.github.com>
2025-03-02 23:41:03 +0700

    Delete src/main/java/com/gymsystem/cyber/IService directory

--- src/main/java/com/gymsystem/cyber/IService/INotify.java
+++
@@ -1,8 +0,0 @@
-package com.gymsystem.cyber.iService;
-
-import com.gymsystem.cyber.entity.Notifications;
-import com.gymsystem.cyber.model.Request.NotificationRequest;
-
-public interface INotify {
-    void saveNotifacation(NotificationRequest notifications);
-}
